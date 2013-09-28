887dc454d0957b527ade0d55f7ca58f2504730c4
anthonyjchriste <anthony.christe@gmail.com>
2013-09-27 15:31:21 -1000

    Added dtuch flag skeleton

---
+++ src/flag/DutchFlag.java
@@ -0,0 +1,27 @@
+package flag;
+
+import java.util.Arrays;
+
+public class DutchFlag {
+
+  private static final int RED = 1;
+  private static final int WHITE = 2;
+  private static final int BLUE = 3;
+
+  private static void swap(int[] a, int i, int j) {
+    int tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+  }
+
+  private static void makeDutchFlag(int[] colors) {
+
+  }
+
+
+  public static void main(String[] args) {
+    int[] colors = {2,1,2,1,3,3,3,3,3,1,2,1,2,1,2,1,1,2,3,2,1,3,1,2,3,3,2,1,1,2,3,1,2};
+    makeDutchFlag(colors);
+    System.out.println(Arrays.toString(colors));
+  }
+}
