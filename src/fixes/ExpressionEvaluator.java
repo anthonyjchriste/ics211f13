8264b002edc77f6ceba41ebdcd49f12648e29f0c
anthonyjchriste <anthony.christe@gmail.com>
2013-10-09 14:40:47 -1000

    Added postfix expression lab code

---
+++ src/fixes/ExpressionEvaluator.java
@@ -0,0 +1,55 @@
+package fixes;
+
+import java.util.Stack;
+
+public class ExpressionEvaluator {
+  /**
+   * Evaluates a postfix expression.
+   * @param postfixExpression An array of Strings, where is string represents either an operator or an operand.
+   * @return The evaluation of this postfix expression.
+   */
+  public static int evaluatePostfix(String[] postfixExpression) {
+    Stack<Integer> operandStack = new Stack<Integer>();
+
+    // TODO: Implement me!
+
+    return 0;
+  }
+
+  /**
+   * Determines if a given string is an operator or not.
+   * @param s The string to test.
+   * @return true if s is an operator, false otherwise.
+   */
+  private static boolean isOperator(String s) {
+    return "+-*/".contains(s);
+  }
+
+  /**
+   * Evaluates an expression given an operator and two operands.
+   * @param operator The operator.
+   * @param operandA The left operand.
+   * @param operandB The right operand.
+   * @return The evaluation of these two operands with the operator.
+   */
+  private static int evaluateWithOperator(String operator, int operandA, int operandB) {
+    switch(operator) {
+      case "+":
+        return operandA + operandB;
+      case "-":
+        return operandA - operandB;
+      case "*":
+        return operandA * operandB;
+      case "/":
+        return operandA / operandB;
+      default:
+        return -1;
+    }
+  }
+
+  public static void main(String[] args) {
+    String[] postfixExpression = {"2", "2", "*", "8", "4", "/", "+"};
+    System.out.println(ExpressionEvaluator.evaluatePostfix(postfixExpression));
+  }
+
+}
